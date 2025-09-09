8abfc2dc9c5a7435aa5a743eddc2df21c992e572
Enzo Schmitt Lima <[enzo.s.lima6@aluno.senai.br]>
2025-09-09 14:14:42 -0300

    Iniciando alterações para o BANCO DE DADOS

--- BACKEND/src/services/category/CreateCategoryService.ts
+++ BACKEND/src/services/category/CreateCategoryService.ts
@@ -1,40 +1,27 @@
 import prismaClient from "../../../prisma";
-interface CategoryRequest {
+interface CategoryRequest{
     name: string;
-    parentId?: string;
 }
 
 class CreateCategoryService {
-    async execute({ name, parentId }: CategoryRequest) {
+    async execute({ name }: CategoryRequest){
 
-        if (!name || name.trim() === '') {
-            throw new Error('Name invalid');
+        if(name === ''){
+            throw new Error('Name invalid')
         }
-        
-        if (parentId) {
-            const parentExists = await prismaClient.category.findUnique({
-                where: { id: parentId },
-            });
 
-            if (!parentExists) {
-                throw new Error('Categoria pai não encontrada');
-            }
-        }
-       
         const category = await prismaClient.category.create({
-            data: {
-                name,
-                parentId: parentId || null, 
+            data:{
+                name: name,
             },
-            select: {
+            select:{
                 id: true,
                 name: true,
-                parentId: true,
-            },
-        });
+            }
+        })
 
         return category;
     }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryService }
