3dfe2f87bd04cb205fbf92acf9bd1c4c408bdda9
Thyriosbatista <thyerrisson.batista@aluno.senai.br>
2025-09-09 14:43:23 -0300

    Instalem npm install express-async-errors

--- BACKEND/src/services/category/CreateCategoryService.ts
+++ BACKEND/src/services/category/CreateCategoryService.ts
@@ -1,35 +1,22 @@
 import prismaClient from "../../../prisma";
 interface CategoryRequest {
     name: string;
-    parentId?: string;
 }
 
 class CreateCategoryService {
-    async execute({ name, parentId }: CategoryRequest) {
+    async execute({ name }: CategoryRequest) {
 
         if (!name || name.trim() === '') {
             throw new Error('Name invalid');
         }
         
-        if (parentId) {
-            const parentExists = await prismaClient.category.findUnique({
-                where: { id: parentId },
-            });
-
-            if (!parentExists) {
-                throw new Error('Categoria pai não encontrada');
-            }
-        }
-       
         const category = await prismaClient.category.create({
             data: {
-                name,
-                parentId: parentId || null, 
+                name
             },
             select: {
                 id: true,
-                name: true,
-                parentId: true,
+                name: true
             },
         });
 
