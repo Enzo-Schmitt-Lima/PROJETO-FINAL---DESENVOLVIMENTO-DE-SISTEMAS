b7ab0e5070e0213e28d9c8b35cff02fb44d9adc6
Enzo Schmitt Lima <[enzo.s.lima6@aluno.senai.br]>
2025-09-30 10:58:31 -0300

    Arrumando categorias

--- BACKEND/src/services/category/CreateCategoryService.ts
+++ BACKEND/src/services/category/CreateCategoryService.ts
@@ -10,26 +10,14 @@ class CreateCategoryService {
         if(name === ''){
             throw new Error('Name invalid')
         }
-
-          if (parentId) {
-            const parentExists = await prismaClient.category.findUnique({
-                where: { id: parentId },
-            });
- 
-            if (!parentExists) {
-                throw new Error('Categoria pai não encontrada');
-            }
-        }
         
         const category = await prismaClient.category.create({
             data: {
                 name,
-                parentId: parentId || null,
             },
             select:{
                 id: true,
-                name: true,
-                parentId: true,
+                name: true
             },
         });
 
