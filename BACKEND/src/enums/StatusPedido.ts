647944a9c521c4b057e2128db694da69555aeb8b
Thyriosbatista <thyerrisson.batista@aluno.senai.br>
2025-10-14 15:51:45 -0300

    Commit pra push

--- BACKEND/src/enums/StatusPedido.ts
+++
@@ -1,7 +0,0 @@
-// src/enums/StatusPedido.ts
-export enum StatusPedido {
-  INDO_PREPARO = 0,
-  PREPARANDO = 1,
-  PRONTO = 2,
-  FINALIZADO = 3,
-}
--- MOBILE/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-import AppRoutes from './app.routes';
-import AuthRoutes from './auth.routes';
-
-export default function Routes() {
-  const { isAuthenticated } = useContext(AuthContext);
-  return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
-}
